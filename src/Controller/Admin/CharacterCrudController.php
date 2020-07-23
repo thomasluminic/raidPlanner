1189644938cfe5c18a5ad5e7c0fe1bff90ef9fe6
thomas <thomas.luminic@outlook.fr>
2020-07-24 00:42:39 +0200

    easy admin ok

---
+++ src/Controller/Admin/CharacterCrudController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\Character;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
+
+class CharacterCrudController extends AbstractCrudController
+{
+    public static function getEntityFqcn(): string
+    {
+        return Character::class;
+    }
+
+
+    public function configureFields(string $pageName): iterable
+    {
+        return [
+            IdField::new('id')->onlyOnIndex(),
+            TextField::new('name', 'Nom'),
+        ];
+    }
+
+    public function configureCrud(Crud $crud): Crud
+    {
+        return $crud
+            // the visible title at the top of the page and the content of the <title> element
+            // it can include these placeholders: %entity_id%, %entity_label_singular%, %entity_label_plural%
+            ->setPageTitle('index', 'Personnages')
+            ;
+    }
+}
